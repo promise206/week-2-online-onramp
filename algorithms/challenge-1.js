411b86f6ad217fea86e73a8287e0e24f9eaf07e3
okpalaugo chukwuka promise <89579386+promise206@users.noreply.github.com>
2022-03-04 07:35:27 +0100

    made changes to challenge 1

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,11 @@
-function missingNumber(numArr) {}
+function missingNumber(numArr) {
+  let missing = [];
+  for (let i = 1; i <= 9; i++) {
+    if (numArr.indexOf(i) === -1) {
+      missing.push(i);
+      console.log(missing);
+    }
+  }
+}
 
-module.exports = missingNumber
+module.exports = missingNumber;
