3dd9ee2e2efc1807fc543fb5bc6d9f03eea8ef44
okpalaugo chukwuka promise <89579386+promise206@users.noreply.github.com>
2022-03-04 10:41:28 +0100

    made changes in challenge 2

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,3 +1,16 @@
-function numSwap(number) {}
+function numSwap(number) {
+  let temp = number.toString();
+  let arrayNum = temp.split("");
+  let arrayNumLength = arrayNum.length / 2;
 
-module.exports = numSwap
+  if (arrayNum.length % 2 === 0) {
+    for (let i = 0; i < arrayNumLength; i++) {
+      [arrayNum[2 * i], arrayNum[2 * i + 1]] = [
+        arrayNum[2 * i + 1],
+        arrayNum[2 * i]
+      ];
+    }
+    return arrayNum;
+  }
+}
+module.exports = numSwap;
