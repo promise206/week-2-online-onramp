5f4bb061820c9630b33099400722937d649403e1
okpalaugo chukwuka promise <89579386+promise206@users.noreply.github.com>
2022-03-04 15:41:27 +0100

    updated

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,3 +1,8 @@
-function increment(arr, value) {}
-
+function increment(arr, value) {
+  let arrLength = arr.length;
+  for(let i = 0; i < arrLength; i++){
+      arr[i]["val"] = arr[i]["val"] + value;
+  }
+  return arr;
+  }
 module.exports = increment;
