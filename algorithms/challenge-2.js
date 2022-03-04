5139f99827c9e86b3994ceb72249668c1717aae4
okpalaugo chukwuka promise <89579386+promise206@users.noreply.github.com>
2022-03-04 10:58:11 +0100

    updated

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,16 +1,18 @@
 function numSwap(number) {
   let temp = number.toString();
   let arrayNum = temp.split("");
-  let arrayNumLength = arrayNum.length / 2;
+  let midLength = arrayNum.length / 2;
 
   if (arrayNum.length % 2 === 0) {
-    for (let i = 0; i < arrayNumLength; i++) {
+    for (let i = 0; i < midLength; i++) {
       [arrayNum[2 * i], arrayNum[2 * i + 1]] = [
         arrayNum[2 * i + 1],
         arrayNum[2 * i]
       ];
     }
     return arrayNum;
+  } else {
+    return "The Length of the  Number must be Even";
   }
 }
 module.exports = numSwap;
