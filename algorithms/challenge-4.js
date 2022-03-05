64dcf1d167212e6edd6e38aba33aa2e5e00f77fd
okpalaugo chukwuka promise <89579386+promise206@users.noreply.github.com>
2022-03-05 07:48:56 +0100

    updated

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,3 +1,9 @@
-function withdraw(amount) {}
-
-module.exports = withdraw
+function withdraw(amount) {
+  let amountLess100 = amount % 100;
+  let hunderedNaira = (amount - amountLess100) / 100;
+  let amountLess50 = amountLess100 % 50;
+  let fiftyNaira = (amountLess100 - amountLess50) / 50;
+  let twentyNaira = amountLess50 / 20;
+  return [hunderedNaira, fiftyNaira, twentyNaira];
+}
+module.exports = withdraw;
