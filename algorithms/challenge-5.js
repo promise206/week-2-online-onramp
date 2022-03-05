c4a50d6a7c33838b816636f35644c3af08dad286
okpalaugo chukwuka promise <89579386+promise206@users.noreply.github.com>
2022-03-05 08:25:27 +0100

    updated

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,3 +1,11 @@
-function arraySum(arr1, arr2) {}
+function arraySum(arr1, arr2) {
+  let temp = [];
+  let sum = [];
+  for (let j = 0; j < arr1.length; j++) {
+    sum = arr1[j] + arr2[j % arr2.length];
+    temp.push(sum);
+  }
+  return temp;
+}
 
-module.exports = arraySum
+module.exports = arraySum;
